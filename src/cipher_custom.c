6cb3a165a725ac406652cfa921ef052cca6ff47f
Ulrich Kreuzeder <ukreuzeder@conpal.de>
2022-06-20 16:47:11 +0200

    Allow properties for custom crypto.

--- src/cipher_custom.c
+++ src/cipher_custom.c
@@ -9,22 +9,27 @@
 
 #include "cipher_common.h"
 
-/* --- AES 256-bit cipher (wxSQLite3) --- */
+/* -- Custom cipher (bring-your-own) --- */
 #if HAVE_CIPHER_CUSTOM
 
 #include "cipher_custom.h"
 
-static int 
-GetLegacyCustomCipher(void* cipher)
-{
-  return 0;
-}
 
-static int
-GetReservedCustomCipher(void* cipher)
+/*
+** Configuration parameters for "custom" cipher
+**
+** - legacy mode : compatibility with System.Data.SQLite encryption
+**                 (page 1 fully encrypted)
+**                 only legacy mode is supported
+**                 possible value:  1 = yes
+*/
+
+SQLITE_PRIVATE CipherParams mcCustomParams[] =
 {
-  return 0;
-}
+  { "legacy", 0, 0, 0, 1},
+  CIPHER_PARAMS_SENTINEL
+};
+
 
 SQLITE_PRIVATE const CipherDescriptor mcCustomDescriptor =
 {
--- src/cipher_custom.h
+++ src/cipher_custom.h
@@ -10,30 +10,34 @@
 #include "cipher_common.h"
 
 /*
-   --- Bring-Your-Own-Cipher --- 
+--- Bring-Your-Own-Cipher ---
 
-   Per default, s
+SQLite3MultipleCiphers has built-in encryption: for AES128, AES256, RC4, ChaCha20, SQLCipher.
+If you want to use your own encryption routines (either if you want to use a different
+algorithm or a different implementation), build with "HAVE_CIPHER_CUSTOM = 1" and:
+
+1. In your own code, implement the external methods below, adding your crypto.
+   You can use the implementation of the builtin algorithms as a template, e.g. cipher_sds_rc4.c.
+
+2. In your own code, set the "cipher" parameter to "CODEC_TYPE_CUSTOM" after opening the DB, e.g.:
+   sqlite3mc_config(m_db, "cipher", CODEC_TYPE_CUSTOM).
+
+NOTE: HAVE:CIPHER_CUSTOM is only supported when creating a static library!
 */
+
 #if HAVE_CIPHER_CUSTOM
 
 /*
-** Example for a cipher structure, to be defined the the user of this functionlity.
-
-typedef struct _CustomCipher
-{
-  int       m_keyLength;
-  uint8_t   m_key[KEY_SIZE_OF_THE_CUSTOM_ALG];
-  void*     m_custom;
-} CustomCipher;
+The external methods below must be implemented by you, when compiling with HAVE_CIPHER_CUSTOM.
 */
-
-/* The following methods must be implemented by the user of this functionality */
+extern int GetLegacyCustomCipher(void* cipher);
+extern int GetPageSizeCustomCipher(void* cipher);
+extern int GetReservedCustomCipher(void* cipher);
 extern void* AllocateCustomCipher(sqlite3* db);
 extern void FreeCustomCipher(void* cipher);
 extern void CloneCustomCipher(void* cipherTo, void* cipherFrom);
-extern int GetPageSizeCustomCipher(void* cipher);
 extern unsigned char* GetSaltCustomCipher(void* cipher);
-extern void GenerateKeyCustomCipher(void* cipher, BtShared* pBt, char* userPassword, int passwordLength, int rekey, unsigned char* cipherSalt);
+extern void GenerateKeyCustomCipher(void* cipher, void* pBt, char* userPassword, int passwordLength, int rekey, unsigned char* cipherSalt);
 extern int EncryptPageCustomCipher(void* cipher, int page, unsigned char* data, int len, int reserved);
 extern int DecryptPageCustomCipher(void* cipher, int page, unsigned char* data, int len, int reserved, int hmacCheck);
 
